1dcb0ffc827874cf3a4cfefb7bcc51b3f3e43840
Josh Keller <JSKeller975@gmail.com>
2023-04-21 15:03:13 -0400

    remove debug

--- controllers/users.js
+++ controllers/users.js
@@ -47,10 +47,7 @@ const createUser = (req, res, next) => {
   const { name, about, avatar, email, password } = req.body;
   bcrypt
     .hash(password, 10)
-    .then((hash) => {
-      debugger;
-      User.create({ name, about, avatar, email, password: hash });
-    })
+    .then((hash) => User.create({ name, about, avatar, email, password: hash }))
     .then((user) => {
       res.status(201).send({
         data: {
